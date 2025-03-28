20ee26838cb08e0fbc8a32bb612467becff5b54e
Nicolas <nicolas.kraide@gamil.com>
2025-03-28 12:17:00 -0300

    add verifyUserAuthorization middleware to a unique route

--- api/src/routes/products.routes.js
+++ api/src/routes/products.routes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const ProductsController = require("../controllers/ProductsController");
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
+const verifyUserAuthorization = require('../middlewares/verifyUserAuthorization')
+
 const productsRoutes = Router();
 
 const productsController = new ProductsController();
@@ -9,6 +11,6 @@ const productsController = new ProductsController();
 productsRoutes.use(ensureAuthenticated);
 
 productsRoutes.get("/", productsController.index);
-productsRoutes.post("/", productsController.create);
+productsRoutes.post("/",  verifyUserAuthorization('admin'), productsController.create);
 
 module.exports = productsRoutes;
