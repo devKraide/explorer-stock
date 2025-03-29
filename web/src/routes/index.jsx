04563aebf7dc46a059817ad44d33b1c0f60921e0
Nicolas <nicolas.kraide@gamil.com>
2025-03-29 09:20:26 -0300

    Implement role-based routing for admin, customer, and sale users

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,33 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLES } from '../utils/roles'
 
+import { CustomerRoutes } from './customer.routes';
 import { AdminRoutes } from './admin.routes';
+import { SaleRoutes } from './sale.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
   const { user } = useAuth();
 
+  function GetRoutes() {
+    switch (user.role) {
+      case USER_ROLES.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLES.CUSTOMER:
+        return <CustomerRoutes />;
+      case USER_ROLES.SALE:
+        return <SaleRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+
+  }
+
   return (
     <BrowserRouter>
-      {user ? <AdminRoutes /> : <AuthRoutes />}
+      {user ? <GetRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
